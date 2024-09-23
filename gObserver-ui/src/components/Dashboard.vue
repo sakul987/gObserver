725cb4b5c89018704c8bf361c55d93e70db717fe
sakul987 <you@example.com>
2024-09-23 21:07:37 +0200

    add dashboard

---
+++ gObserver-ui/src/components/Dashboard.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const count = ref(0)
+const cpu_usage = ref(0)
+const cpu_temp = ref(0)
+const ssd_temp = ref(0)
+const ssd_size = ref(0)
+const ssd_used = ref(0)
+const ssd_used_perc = ref(0)
+const ssd_available = ref(0)
+const ram_size = ref(0)
+const ram_used = ref(0)
+const ram_used_perc = ref(0)
+const ram_available = ref(0)
+
+onMounted(() => {
+    cpu_usage.value = 10
+})
+
+</script>
+
+<template>
+    <div class="grid grid-cols-1 gap-36 sm:grid-cols-2 md:grid-cols-3">
+        <div>
+            <h1>CPU</h1>
+            <div class="grid grid-cols-2 gap-6">
+                <div>
+                    <h2>Usage</h2>
+                    <div>{{cpu_usage}}%</div>
+                </div>
+                <div>
+                    <h2>Temp</h2>
+                    <div>{{cpu_temp}}°C</div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <h1>RAM</h1>
+            <div class="grid grid-cols-2 gap-6">
+                <div>
+                    <h2>Usage</h2>
+                    <div>{{ram_used}} GiB</div>
+                </div>
+                <div>
+                    <h2>Usage %</h2>
+                    <div>{{ram_used_perc}}%</div>
+                </div>
+                <div>
+                    <h2>Size</h2>
+                    <div>{{ram_size}} GiB</div>
+                </div>
+                <div>
+                    <h2>Available</h2>
+                    <div>{{ram_available}} GiB</div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <h1>SSD</h1>
+            <div class="grid grid-cols-2 gap-6">
+                <div>
+                    <h2>Temp</h2>
+                    <div>{{ssd_temp}}°C</div>
+                </div>
+                <div>
+                    <h2>Usage %</h2>
+                    <div>{{ssd_used_perc}}%</div>
+                </div>
+                <div>
+                    <h2>Size</h2>
+                    <div>{{ssd_size}} GiB</div>
+                </div>
+                <div>
+                    <h2>Available</h2>
+                    <div>{{ssd_available}} GiB</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
