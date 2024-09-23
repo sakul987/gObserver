90b590a484193234478ba907c1682be981332b89
sakul987 <you@example.com>
2024-09-23 18:37:34 +0200

    use https

--- gObserver-ui/vite.config.ts
+++ gObserver-ui/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import fs from 'fs'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'vite.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'vite.crt')),
+    },
+    port: 3000,
+  },
 })
